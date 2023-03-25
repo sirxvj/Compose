64a8e4afb36b56e755d70f1f58f27a89a6b7aa36
sirxvj <bekman2004@mail.ru>
2023-03-25 13:11:05 +0300

    Initial commit

--- app/src/main/java/com/example/compose/data/remote/test/StudentGroup.kt
+++
@@ -1,9 +0,0 @@
-package com.example.compose.data.remote.test
-
-data class StudentGroup(
-    val educationDegree: Int,
-    val name: String,
-    val numberOfStudents: Int,
-    val specialityCode: String,
-    val specialityName: String
-)
